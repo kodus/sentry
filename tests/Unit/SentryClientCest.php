3885d405d8f94542a0f948c319360fca2dd00982
Hans Erik Jepsen <haeje@jfmedier.dk>
2022-08-18 10:46:38 +0200

    Set up Codeception

---
+++ tests/Unit/SentryClientCest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace Tests\Unit;
+
+use Tests\Support\UnitTester;
+
+class SentryClientCest
+{
+    public function _before(UnitTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(UnitTester $I)
+    {
+    }
+}
