54f48a33a874694fe8fd7e1e43286551dbb6741d
m1ke <git@m1ke.me>
2024-02-28 10:39:06 +0000

    New callable extension added

---
+++ src/Extensions/CallableExtension.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Kodus\Sentry\Extensions;
+
+use Throwable;
+use Kodus\Sentry\Model\Event;
+use Kodus\Sentry\SentryClientExtension;
+use Psr\Http\Message\ServerRequestInterface;
+
+class CallableExtension implements SentryClientExtension
+{
+	/**
+	 * @var Closure(Event, Throwable, ServerRequestInterface|null):void
+	 */
+	private $callable;
+
+	/**
+	 * @param Closure(Event, Throwable, ServerRequestInterface|null):void $callable
+	 */
+	public function __construct(callable $callable) {
+		$this->callable = $callable;
+	}
+
+	public function apply(Event $event, Throwable $exception, ?ServerRequestInterface $request): void {
+		$callable = $this->callable;
+		$callable($event, $exception, $request);
+	}
+}
